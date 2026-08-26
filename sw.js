e71d06312005864f42c9d15440529951b3bce90e
murielsousa8 <murielsousa8@gmail.com>
2026-08-26 12:00:03 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,36 @@
+const CACHE_NAME = 'painel-na-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  // Ignora requisições de API externas do cache
+  if (event.request.url.includes('benuvem.com.br')) {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
